7f47925ace2bba25ef5ad30d0c52c3792e99233a
leelee1995 <thevanglee@gmail.com>
2025-12-06 21:53:31 -0600

    updated

--- src/constant/envs.ts
+++ src/constant/envs.ts
@@ -2,6 +2,6 @@ const getValue = (key: string) => {
 	return process.env[key];
 };
 
-export const PORT = getValue("PORT") || 3001;
+export const PORT = getValue("PORT") || 3000;
 export const RENDER_TOKEN = getValue("RENDER_TOKEN");
 export const MODE = getValue("MODE");
